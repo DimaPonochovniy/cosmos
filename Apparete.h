95584324d7ad3ce8986fcc9f91b6ea1be35e563a
DimaPonochovniy <dima_ponochovniy@mail.ru>
2013-05-20 15:53:19 +0300

    Create Apparete.h

---
+++ Apparete.h
@@ -0,0 +1,19 @@
+#include <string>
+using namespace std;
+
+class Apparete
+{
+private:
+  string name;
+int Number;
+int mass;
+public:
+	Apparete();
+	Apparete(int Number);
+	
+void setApparete ();
+	
+int get();
+
+void DIsplay();
+};
