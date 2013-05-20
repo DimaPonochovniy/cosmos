93c334cab8da869dbc5f29eb2a76ba43748d7a32
DimaPonochovniy <dima_ponochovniy@mail.ru>
2013-05-20 15:54:51 +0300

    Create Apparete1.cpp

---
+++ Apparete1.cpp
@@ -0,0 +1,22 @@
+// DimaPonochovniy.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <conio.h>
+#include <iostream>
+#include <string>
+#include "Apparete.h"
+using namespace std;
+
+
+
+
+int _tmain(int argc, _TCHAR* argv[])
+
+{
+  Apparete myApparete;
+
+	return 0;
+}
+
