074d02f54ac019b74d3e63832ba042a9d0f69911
DimaPonochovniy <dima_ponochovniy@mail.ru>
2013-05-20 15:53:47 +0300

    Create Apparete.cpp

---
+++ Apparete.cpp
@@ -0,0 +1,20 @@
+#include "Apparete.h"
+
+Apparete::Apparete(){
+
+}
+Apparete::Apparete(int Number){
+
+}
+  
+void Apparete::setApparete (){
+
+}
+	
+int Apparete::get(){
+
+}
+
+void Apparete::DIsplay(){
+
+}
